83b08383bd05d91b4cf30a080ec4f49a5c9a2f85
riwebstore <ri98.eood@gmail.com>
2021-02-05 14:37:48 +0200

    Audit logs table creation
    - creating prototype of audit logs table
    - inserting the columns and their specifications

--- server/src/models.py
+++ server/src/models.py
@@ -29,3 +29,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {} {} {}>'.format(self.id, self.username, self.role)
+
+class AuditLogs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(80), nullable=False)
+    result = db.Column(db.String(80), nullable=False)
+ 
+    def __repr__(self):
+        return '<Audit Logs {} {} {}>'.format(self.id, self.user, self.result)
+   
